6072efa69043ddf766f16f2cdc313bfd4a7c0e1c
Miles Burton <miles.burton@agileview.co.uk>
2017-08-05 13:14:19 +0100

    Added line command

--- src/main/scala/CommandInterpreter.scala
+++ src/main/scala/CommandInterpreter.scala
@@ -1,7 +1,8 @@
 class CommandInterpreter() {
 
-
   val isCreateCanvasStringRegEx = "^C\\s(\\d+)\\s(\\d+)$".r
+
+  val isDrawLineStringRegEx = "^L\\s(\\d+)\\s(\\d+)\\s(\\d+)\\s(\\d+)$".r
 
   def interpret(userInput: String) = {
 
@@ -9,6 +10,10 @@ class CommandInterpreter() {
 
       case isCreateCanvasStringRegEx(width, height) =>
         CreateCanvasCommand(width.toInt, height.toInt)
+
+      case isDrawLineStringRegEx(x1, y1, x2, y2) =>
+        DrawLineCommand(x1.toInt, y1.toInt, x2.toInt, y2.toInt)
+
       case _ =>
         None
 
@@ -20,3 +25,5 @@ class CommandInterpreter() {
 }
 
 case class CreateCanvasCommand(width: Int, height: Int)
+
+case class DrawLineCommand(x1: Int, y1: Int, x2: Int, y2: Int)
--- src/test/scala/CommandInterpreterSpec.scala
+++ src/test/scala/CommandInterpreterSpec.scala
@@ -21,9 +21,29 @@ class CommandInterpreterSpec extends FlatSpec with Matchers {
 
   }
 
-  it should "not crash if create canvas input it invalid" in {
+  it should "gracefully handle invalid create canvas input" in {
 
     val userInput = "C 20.5 4.6"
+
+    val command = new CommandInterpreter().interpret(userInput)
+
+    command should be(None)
+
+  }
+
+  it should "return draw line command" in {
+
+    val userInput = "L 1 2 6 2"
+
+    val command = new CommandInterpreter().interpret(userInput)
+
+    command should be(DrawLineCommand(1,2,6,2))
+
+  }
+
+  it should "gracefully handle invalid draw line input" in {
+
+    val userInput = "L 1 A 2 6.5 2"
 
     val command = new CommandInterpreter().interpret(userInput)
 
